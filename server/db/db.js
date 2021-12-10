5a4e29b68e784b59ed55616c4ac00dea6849a786
TJ <tjdrzewiecki@gmail.com>
2021-12-10 02:12:00 -0500

    Trying to get this github actions db thing to work

--- server/db/db.js
+++ server/db/db.js
@@ -1,10 +1,17 @@
 const Sequelize = require("sequelize");
 require('dotenv').config();
 
-const db = new Sequelize(process.env.DATABASE_URL || "messenger", process.env.SECRET_USERNAME, process.env.SECRET_PASSWORD, {
+const db = new Sequelize(process.env.DATABASE_URL);
+
+module.exports = db;
+
+
+/*
+
+ || "messenger", process.env.SECRET_USERNAME, process.env.SECRET_PASSWORD, {
   host: 'localhost',
   logging: false,
   dialect: 'postgres',
-});
+}
 
-module.exports = db;
+*/
