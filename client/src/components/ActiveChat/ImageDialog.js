35da377684f672d80fdf132110ea44ef62755424
TJ <tjdrzewiecki@gmail.com>
2021-12-11 14:02:58 -0500

    Deleted unused file

--- client/src/components/ActiveChat/ImageDialog.js
+++
@@ -1,5 +0,0 @@
-/*
-Button in Input that opens this dialog box modal and this takes a url that on submit sends to Input.js to be sent in reqBody as attachment, so if attachment exists send it, etc.
-
-Look into material-ui icons for a button to put in Input?
-*/
