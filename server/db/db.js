0fbc8c051d6af5e169397f01dfba28c84fef0996
TJ <tjdrzewiecki@gmail.com>
2021-12-11 15:05:02 -0500

    db updated for use

--- server/db/db.js
+++ server/db/db.js
@@ -1,7 +1,11 @@
 const Sequelize = require("sequelize");
 require('dotenv').config();
 
-const db = new Sequelize(process.env.DATABASE_URL);
+const db = new Sequelize(process.env.DATABASE_URL || "messenger", process.env.SECRET_USERNAME, process.env.SECRET_PASSWORD, {
+  host: 'localhost',
+  logging: false,
+  dialect: 'postgres',
+});
 
 module.exports = db;
 
