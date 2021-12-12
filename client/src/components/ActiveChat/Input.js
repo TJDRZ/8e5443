727422234aff4304636e66a485cbdf5a15a45553
TJ <tjdrzewiecki@gmail.com>
2021-12-12 18:04:11 -0500

    UI updates

--- client/src/components/ActiveChat/Input.js
+++ client/src/components/ActiveChat/Input.js
@@ -1,5 +1,11 @@
 import React, { useState, useRef } from "react";
-import { Grid, FormControl, FilledInput, Typography } from "@material-ui/core";
+import {
+  Grid,
+  FormControl,
+  FilledInput,
+  Button,
+  Typography,
+} from "@material-ui/core";
 import ImageIcon from "@material-ui/icons/Image";
 import { makeStyles } from "@material-ui/core/styles";
 import { connect } from "react-redux";
@@ -21,24 +27,15 @@ const useStyles = makeStyles(() => ({
 const Input = (props) => {
   const classes = useStyles();
   const [text, setText] = useState("");
-  const [imgUrl, setImgUrl] = useState("");
+  const [previewSource, setPreviewSource] = useState();
+  const [imgURL, setImgURL] = useState();
   const [attachments, setAttachments] = useState([]);
   const imgInput = useRef(null);
   const imgAttachTypo = useRef(null);
   const { postMessage, otherUser, conversationId, user } = props;
 
-  const toggleImgInput = () => {
-    imgInput.current.style.display === "none"
-      ? (imgInput.current.style.display = "block")
-      : (imgInput.current.style.display = "none");
-  };
-
   const handleTextChange = (event) => {
     setText(event.target.value);
-  };
-
-  const handleImgChange = (event) => {
-    setImgUrl(event.target.value);
   };
 
   const handleTextSubmit = async (event) => {
@@ -54,33 +51,88 @@ const Input = (props) => {
     await postMessage(reqBody);
     await props.fetchConvos();
     setText("");
+    setPreviewSource();
     setAttachments([]);
     imgAttachTypo.current.style.display = "none";
     imgInput.current.style.display = "none";
   };
 
-  const handleImgSubmit = (event) => {
+  const toggleImgInput = () => {
+    imgInput.current.style.display === "none"
+      ? (imgInput.current.style.display = "block")
+      : (imgInput.current.style.display = "none");
+  };
+
+  const handleImgChange = async (event) => {
+    const file = event.target.files[0];
+    const reader = new FileReader();
+    reader.readAsDataURL(file);
+    reader.onloadend = () => {
+      setPreviewSource(reader.result);
+    };
+
+    const formData = new FormData();
+    formData.append("file", file);
+    formData.append("upload_preset", "gglru5ny");
+    await fetch("https://api.cloudinary.com/v1_1/tjdrz/image/upload", {
+      method: "POST",
+      body: formData,
+      mode: "cors",
+    })
+      .then((res) => res.json())
+      .then((res) => setImgURL(res.secure_url))
+      .catch((err) => console.log(err));
+  };
+
+  const handleImgSubmit = async (event) => {
     event.preventDefault();
-    setAttachments([...attachments, imgUrl]);
-    setImgUrl("");
+    if (!previewSource) return;
+    setPreviewSource();
+    setAttachments([...attachments, imgURL]);
+    imgInput.current[0].value = "";
+    imgInput.current.style.display = "none";
     imgAttachTypo.current.style.display = "block";
   };
 
   return (
     <Grid>
-      <form ref={imgInput} className={classes.root} style={{ display: "none" }} onSubmit={handleImgSubmit}>
+      {previewSource && (
+        <img
+          src={previewSource}
+          alt="Chosen"
+          style={{ width: "10rem", height: "10rem" }}
+        />
+      )}
+      <form
+        ref={imgInput}
+        className={classes.root}
+        style={{ display: "none" }}
+        onSubmit={handleImgSubmit}
+      >
         <FormControl fullWidth hiddenLabel style={{ position: "relative" }}>
           <FilledInput
             classes={{ root: classes.input }}
             disableUnderline
-            placeholder="Type image url..."
-            value={imgUrl}
-            name="imgUrl"
-            onChange={handleImgChange} 
+            type="file"
+            name="imgFile"
+            onChange={handleImgChange}
           />
         </FormControl>
+        <Button
+          type="submit"
+          variant="contained"
+          size="large"
+          color="primary"
+          disableElevation
+          style={{ width: "10rem" }}
+        >
+          Attach
+        </Button>
       </form>
-      <Typography ref={imgAttachTypo} variant="h6" style={{ display: "none" }}>Image(s) Attached!</Typography>
+      <Typography ref={imgAttachTypo} variant="h6" style={{ display: "none" }}>
+        Image(s) Attached:
+      </Typography>
+      {attachments.map((img, index) => {return <img key={index} src={img} alt="Attached" style={{ width: "10rem", height: "10rem", margin: "1rem" }}/>})}
       <form className={classes.root} onSubmit={handleTextSubmit}>
         <FormControl fullWidth hiddenLabel style={{ position: "relative" }}>
           <FilledInput
